42936a69689b461a44892060c01dcf164e526a6a
PronayMallick <callpronay@gmail.com>
2024-03-11 10:55:06 +0530

    11 commit

--- components/tarotOne.js
+++ components/tarotOne.js
@@ -79,8 +79,8 @@ const TarotOne =  () => {
 
             <div className='flex flex-col h-56 relative items-center bg-slate-200'>
                 <div className=' w-56 h-20 flex relative'>
-                    <button className='absolute left-0 tarotCard w-14 h-20 rounded-md filter' id='card-1' onClick={() => selectCard('card-1')}>{tarotImg}</button>
-                    <button className='absolute left-4 tarotCard w-14 h-20 rounded-md filter' id='card-2' onClick={() => selectCard('card-2')}>{tarotImg}</button>
+                    <button className='absolute left-0 tarotCard w-14 h-20 rounded-md' id='card-1' onClick={() => selectCard('card-1')}>{tarotImg}</button>
+                    <button className='absolute left-4 tarotCard w-14 h-20 rounded-md' id='card-2' onClick={() => selectCard('card-2')}>{tarotImg}</button>
                     <button className='absolute left-8 tarotCard w-14 h-20 rounded-md' id='card-3' onClick={() => selectCard('card-3')}>{tarotImg}</button>
                     <button className='absolute left-12 tarotCard w-14 h-20 rounded-md' id='card-4' onClick={() => selectCard('card-4')}>{tarotImg}</button>
                     <button className='absolute left-16 tarotCard w-14 h-20 rounded-md' id='card-5 ' onClick={() => selectCard('card-5')}>{tarotImg}</button>
@@ -92,21 +92,21 @@ const TarotOne =  () => {
                     <button className='absolute left-40 tarotCard w-14 h-20 rounded-md' id='card-11'  onClick={() => selectCard('card-11')}>{tarotImg}</button>
                 </div>
                 <div className='w-52 h-20 flex relative bottom-12'>
-                    <button className='absolute left-0 tarotCard w-14 h-20  bg-blue-300 hover:bg-blue-200 rounded-md' id='card-12' onClick={() => selectCard('card-12')}>{tarotImg}</button>
-                    <button className='absolute left-4 tarotCard w-14 h-20  bg-rose-300 hover:bg-rose-200 rounded-md' id='card-13'  onClick={() => selectCard('card-13')}>{tarotImg}</button>
-                    <button className='absolute left-8 tarotCard w-14 h-20  bg-green-300 hover:bg-green-200 rounded-md' id='card-14' onClick={() => selectCard('card-14')}>{tarotImg}</button>
-                    <button className='absolute left-12 tarotCard w-14 h-20  bg-pink-300 hover:bg-pink-200 rounded-md' id='card-15' onClick={() => selectCard('card-15')}>{tarotImg}</button>
-                    <button className='absolute left-16 tarotCard w-14 h-20  bg-orange-300 hover:bg-orange-200 rounded-md' id='card-16' onClick={() => selectCard('card-16')}>{tarotImg}</button>
-                    <button className='absolute left-20 tarotCard w-14 h-20  bg-indigo-300 hover:bg-indigo-200 rounded-md' id='card-17' onClick={() => selectCard('card-17')}>{tarotImg}</button>
-                    <button className='absolute left-24 tarotCard w-14 h-20  bg-green-300 hover:bg-green-200 rounded-md' id='card-18' onClick={() => selectCard('card-18')}>{tarotImg}</button>
-                    <button className='absolute left-28 tarotCard w-14 h-20  bg-red-300 hover:bg-red-200 rounded-md' id='card-19' onClick={() => selectCard('card-19')}>{tarotImg}</button>
-                    <button className='absolute left-32 tarotCard w-14 h-20  bg-blue-300 hover:bg-blue-200 rounded-md' id='card-20' onClick={() => selectCard('card-20')}>{tarotImg}</button>
-                    <button className='absolute left-36 tarotCard w-14 h-20  bg-teal-300 hover:bg-teal-200 rounded-md' id='card-21' onClick={() => selectCard('card-21')}>{tarotImg}</button>
-                    <button className='absolute left-40 tarotCard w-14 h-20  bg-fuchsia-300 hover:bg-fuchsia-200 rounded-md' id='card-22' onClick={() => selectCard('card-22')}>{tarotImg}</button>          
+                    <button className='absolute left-0 tarotCard w-14 h-20  rounded-md' id='card-12' onClick={() => selectCard('card-12')}>{tarotImg}</button>
+                    <button className='absolute left-4 tarotCard w-14 h-20  rounded-md' id='card-13'  onClick={() => selectCard('card-13')}>{tarotImg}</button>
+                    <button className='absolute left-8 tarotCard w-14 h-20  rounded-md' id='card-14' onClick={() => selectCard('card-14')}>{tarotImg}</button>
+                    <button className='absolute left-12 tarotCard w-14 h-20 rounded-md' id='card-15' onClick={() => selectCard('card-15')}>{tarotImg}</button>
+                    <button className='absolute left-16 tarotCard w-14 h-20 rounded-md' id='card-16' onClick={() => selectCard('card-16')}>{tarotImg}</button>
+                    <button className='absolute left-20 tarotCard w-14 h-20 rounded-md' id='card-17' onClick={() => selectCard('card-17')}>{tarotImg}</button>
+                    <button className='absolute left-24 tarotCard w-14 h-20 rounded-md' id='card-18' onClick={() => selectCard('card-18')}>{tarotImg}</button>
+                    <button className='absolute left-28 tarotCard w-14 h-20 rounded-md' id='card-19' onClick={() => selectCard('card-19')}>{tarotImg}</button>
+                    <button className='absolute left-32 tarotCard w-14 h-20 rounded-md' id='card-20' onClick={() => selectCard('card-20')}>{tarotImg}</button>
+                    <button className='absolute left-36 tarotCard w-14 h-20 rounded-md' id='card-21' onClick={() => selectCard('card-21')}>{tarotImg}</button>
+                    <button className='absolute left-40 tarotCard w-14 h-20 rounded-md' id='card-22' onClick={() => selectCard('card-22')}>{tarotImg}</button>          
                 </div>
                 <div className='relative'>
                     {/* {cardSelect} */}
-                    <button id='dispCard' className=' py-2 px-3 rounded-lg text-sm bg-red-600' onClick={() => displayCard() }> {(cardLeft < 0) ? "Show my reading" : cards ? "Choose "+ cardLeft +" more cards" : "Select 1 cards"}</button>
+                    <button id='dispCard' className=' py-2 px-3 rounded-lg text-sm bg-orange-500' onClick={() => displayCard() }> {(cardLeft < 0) ? "Show my reading" : cards ? "Choose "+ cardLeft +" more cards" : "Select 1 cards"}</button>
                 </div>           
             </div>
 
